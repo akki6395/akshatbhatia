7a6e5ec9264e2b56fc3adb567140590ab73de490
akki6395 <akshatbhatia6395@gmail.com>
2024-12-28 14:31:09 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    printf("HAPPY NEW YEAR 2025\n");
+  printf("May god give you a good health\n");
+  printf("may your all wishes fullfil");
+  getch();
+}
